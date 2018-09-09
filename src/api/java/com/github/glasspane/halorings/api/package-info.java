3c9d449de6491c4be490087eee7ef74c95ccf688
UpcraftLP <j-schirra@gmx.de>
2018-09-09 02:21:13 +0200

    api package-info

---
+++ src/api/java/com/github/glasspane/halorings/api/package-info.java
@@ -0,0 +1,8 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@API(owner = "halo_rings", provides = "Halo Rings|API", apiVersion = "0.1.0")
+package com.github.glasspane.halorings.api;
+
+import mcp.MethodsReturnNonnullByDefault;
+import net.minecraftforge.fml.common.API;
+import javax.annotation.ParametersAreNonnullByDefault;
